39ae58a475250d06ec127bf24b4bfc8585382b81
chengyao.xu_windows <chengyao.xu@bridgewell.com>
2021-09-22 17:55:26 +0800

    [feature] Add state and methods into context

--- src/contexts/MessageContext.js
+++ src/contexts/MessageContext.js
@@ -2,17 +2,66 @@ import React from 'react';
 
 const Context = React.createContext('');
 
-export class LanguageStore extends React.Component {
-  state = { language: 'english' };
+export class MessageStore extends React.Component {
+  state = { 
+    messages: [
+      {id: 1, text: 'walk the dog', status: 'done'},
+      {id: 2, text: 'clean the house', status: 'done'},
+      {id: 4, text: 'buy the textbook', status: 'pending'},
+      {id: 5, text: 'buy a new phone', status: 'pending'},
+      {id: 6, text: 'mission impossible', status: 'pending'},
+      {id: 7, text: 'coding something', status: 'doing'},
+      {id: 8, text: 'become a billionare', status: 'open'},
+      {id: 9, text: 'buy a tesla', status: 'open'},
+    ], 
+    counter: 10
+  };  
 
-  onLanguageChange = language => {
-    this.setState({ language });
+  onAddMessage = (text, status) => {
+    this.setState({
+        messages: [...this.state.messages, {
+            id: this.state.counter,
+            text: text,
+            status: status
+        }],
+        counter: this.state.counter + 1
+    });
+  };
+
+  onDeleteMessage = (id) => {
+    let newMessages = this.state.messages.concat().filter(x => x.id !== id);
+    this.setState({
+        messages: newMessages,
+        counter: this.state.counter
+    });
+  };
+
+  onUpdateMessage = (id, text, status) => {
+    let newMessages = this.state.messages.concat().map((x) => {
+        if (x.id !== id) {
+            return x;
+        } else {
+            return {
+                id: id,
+                text: text,
+                status: status
+            }
+        }
+    });
+    this.setState({
+        messages: newMessages,
+        counter: this.state.counter
+    });
   };
 
   render() {
     return (
       <Context.Provider
-        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+        value={{ ...this.state, 
+            onAddMessage: this.onAddMessage,
+            onDeleteMessage: this.onDeleteMessage,
+            onUpdateMessage: this.onUpdateMessage,
+        }}
       >
         {this.props.children}
       </Context.Provider>
