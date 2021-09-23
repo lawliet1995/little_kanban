7325e96c9098c763dc1b13a75efb49c1b7e1e052
chengyao.xu_windows <chengyao.xu@bridgewell.com>
2021-09-23 14:40:27 +0800

    [feature] Apply context consumer and provider

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import styles from "./App.module.css";
 import KanbanRow from './components/KanbanRow/KanbanRow';
 import EditModal from './components/EditModal/EditModal';
 import InputField from './components/InputField/InputField';
+import {MessageStore} from './contexts/MessageContext';
+import MessageContext from './contexts/MessageContext';
 
 class App extends React.Component {
-  constructor (props){
-    super(props);
-    this.state = { 
-      messages: [
-        {id: 1, text: 'walk the dog', status: 'done'},
-        {id: 2, text: 'clean the house', status: 'done'},
-        {id: 4, text: 'buy the textbook', status: 'pending'},
-        {id: 5, text: 'buy a new phone', status: 'pending'},
-        {id: 6, text: 'mission impossible', status: 'pending'},
-        {id: 7, text: 'coding something', status: 'doing'},
-        {id: 8, text: 'become a billionare', status: 'open'},
-        {id: 9, text: 'buy a tesla', status: 'open'},
-      ], 
-      counter: 10
-    };  
-  }
+
   render() {
     return (
-      <React.Fragment>
+      <MessageStore>
         <div className="ui container">
           <div className = {styles.headerWrapper} >
             <h1 id = {styles.kanbanHeader} >Simple Kanban</h1>
@@ -33,15 +20,23 @@ class App extends React.Component {
 
           <div className="ui divider" ></div>
           <div className={styles.kanban}>
-              <KanbanRow messages={this.state.messages.filter(x => x.status === 'open')} title="Open" color="#F0AD4E"></KanbanRow>
-              <KanbanRow messages={this.state.messages.filter(x => x.status === 'doing')} title= "Doing" color="#5CB85C"></KanbanRow>
-              <KanbanRow messages={this.state.messages.filter(x => x.status === 'pending')} title= "Pending" color="#00CCFF"></KanbanRow>
-              <KanbanRow messages={this.state.messages.filter(x => x.status === 'done')} title= "Done" color="#FE9A76"></KanbanRow>
+            <MessageContext.Consumer>
+              {({messages}) => {
+                return (
+                  <React.Fragment>
+                    <KanbanRow messages={messages.filter(x => x.status === 'open')} title="Open" color="#F0AD4E"></KanbanRow>
+                    <KanbanRow messages={messages.filter(x => x.status === 'doing')} title= "Doing" color="#5CB85C"></KanbanRow>
+                    <KanbanRow messages={messages.filter(x => x.status === 'pending')} title= "Pending" color="#00CCFF"></KanbanRow>
+                    <KanbanRow messages={messages.filter(x => x.status === 'done')} title= "Done" color="#FE9A76"></KanbanRow>
+                  </React.Fragment>                
+                );
+              }}
+            </MessageContext.Consumer>          
           </div>
         </div>
 
         <EditModal />
-      </React.Fragment>
+      </MessageStore>
 
     );
   }
--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import styles from "./InputField.module.css";
-
+import MessageContext from '../../contexts/MessageContext';
 class InputField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curText: '',
+            curState: 'open',
+        };
+    }
+    static contextType = MessageContext;
+
     handleSubmit = (e) => {
         e.preventDefault();
         e.stopPropagation();
-        console.log('i am click');
+        this.context.onAddMessage(this.state.curText, this.state.curState);
     }
 
-    handleTyping(e) {
-        console.log('typing...');
+    handleTyping = (e) => {
+        this.setState({...this.state, curText: e.target.value});
     }
 
-    handleSelect(e) {
-        console.log('Select!');
+    handleSelect = (e) => {
+        this.setState({...this.state, curState: e.target.value});
     }
 
-    render (){
+    render () {
         return (
             <form className="ui form" onSubmit={this.handleSubmit}>
                 <div className="field">
@@ -25,7 +34,8 @@ class InputField extends React.Component {
                     <div className={styles.flexContainer}>
                         <input id={styles.textInput} type="text" 
                             name="task-name" placeholder="write task here"
-                            onChange={this.handleTyping}></input>
+                            onChange={this.handleTyping}>
+                        </input>
 
                         <select id={styles.selectLabel} className="ui dropdown" onChange={this.handleSelect}>
                             <option value="open">open</option>
